9ca6fc5f903e466efea48273a96bcfe1ce10c18f
Maulana Imanulhaq <imanulhaq5@gmail.com>
2024-07-14 11:11:06 +0700

    library

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import streamlit as st
-from streamlit_option_menu import option_menu
 import tensorflow as tf
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
